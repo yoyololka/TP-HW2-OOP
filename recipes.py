2079bbeb1634b52824cb15f5c99766a5bc2007b7
yoyoloxxx <g.glazirin0000@gmail.com>
2026-06-04 01:50:37 +0300

    Добавил класс DietaryRecipe

--- recipes.py
+++ recipes.py
@@ -59,3 +59,16 @@ class Recipe:
         for ing in self.ingredients:
             result += "  - " + str(ing) + "\n"
         return result
+
+
+class DietaryRecipe(Recipe):
+    def __init__(self, title: str, diet_type: str, ingredients: list = None):
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
+
+    def scale(self, ratio: float):
+        scaled = super().scale(ratio)
+        return DietaryRecipe(scaled.title, self.diet_type, scaled.ingredients)
+
+    def __str__(self):
+        return f"[{self.diet_type}] " + super().__str__()
